b6f7ee92b199bb76f24b7f30bee6a5a360d9055a
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2014-12-28 16:22:33 -0200

    Adding edition show.

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -2,4 +2,8 @@ class EditionsController < ApplicationController
 	def index
 		@editions = Edition.all
 	end
+	def show
+		@edition = Edition.find(params[:id])
+		# @other_editions = Edition.find_by_work_id(@edition.work_id) insersect with edition
+	end
 end
