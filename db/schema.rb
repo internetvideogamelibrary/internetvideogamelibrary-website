9738af955392c700f49a8bf1ced56dc34987b093
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-01-02 17:30:47 -0200

    Renaming column of game shelves.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102190503) do
+ActiveRecord::Schema.define(version: 20150102192922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20150102190503) do
   create_table "game_shelves", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.integer  "type"
+    t.integer  "shelf_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
