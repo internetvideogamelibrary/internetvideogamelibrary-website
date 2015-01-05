5d0f26e1a7e366d18350b8f178dce1cd6838d224
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-01-05 18:59:57 -0200

    Changing exporter to export genre.

--- utils/scrapy/steamspider/steamspider/edition_seeds_item_exporter.py
+++ utils/scrapy/steamspider/steamspider/edition_seeds_item_exporter.py
@@ -27,10 +27,14 @@ class EditionSeedsItemExporter(BaseItemExporter):
         self.file.write('work = Work.create(:original_title => "%s", :original_release_date => \'%s\')\n' % (itemdict['title'].replace('&amp;','&').replace('"', r'\"'), release_date))
         self.file.write("edition = Edition.create(:media_id => media.id, :region_id => region.id, :platform_id => platform.id, :work_id => work.id, :description => \"%s\", :release_date => \"%s\", :title => \"%s\", :developer => \"%s\", :publisher => \"%s\", :coverart_remote_url => '%s')\n" % (itemdict['description'].replace('\r','\n').replace('&amp;','&').replace('\\', '\\\\').replace('"', r'\"').replace('\t', '').strip(), release_date, itemdict['title'].replace('&amp;','&').replace('"', r'\"'), itemdict['developer'].replace('"', r'\"'), itemdict['publisher'].replace('"', r'\"'), itemdict['image_url']) )
         for genre in itemdict['genres']:
-            self.file.write('#genre = Genre.find_by_title("%s")\n' % genre)
-            self.file.write('#if genre.present?\n')
-            self.file.write('#\tedition.add_genre(genre)\n')
-            self.file.write('#end\n')
+            self.file.write('genre = Genre.find_by_title("%s")\n' % genre)
+            self.file.write('if genre.present?\n')
+            self.file.write('\tedition.genres << genre\n')
+            self.file.write('end\n')
 
         for platform in itemdict['platforms']:
             self.file.write('#edition.add_parameter(:platform => "%s")\n' % platform)
+
+        self.file.write('work = nil\n')
+        self.file.write('genre = nil\n')
+        self.file.write('edition = nil\n')
