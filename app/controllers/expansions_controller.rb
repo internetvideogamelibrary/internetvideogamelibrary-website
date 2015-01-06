d6ad1e1d0ab7e0f923c5b488bc8aecfeef9ff274
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-01-06 16:34:53 -0200

    Add expansion is now working.

--- app/controllers/expansions_controller.rb
+++ app/controllers/expansions_controller.rb
@@ -1,8 +1,39 @@
 class ExpansionsController < ApplicationController
+	before_filter :authenticate_user!,
+	:only => [:new, :create, :edit, :update]
 	def create
+		@edition = Edition.find_by_id(params[:edition_id])
+		if @edition == nil
+			redirect_to :back, :alert => "Game not found"
+		end
+
+		@expansion = Expansion.new(expansion_params)
+		@expansion.edition = @edition
+
+		if @expansion.save
+			flash[:notice] = "DLC/Expansion was added successfully."
+			redirect_to @edition
+		else
+			render 'new'
+		end
+
+		rescue ActionController::RedirectBackError
+			redirect_to '/', :alert => "Game not found"
 	end
 	def new
 		@edition = Edition.find_by_id(params[:edition_id])
+		if @edition == nil
+			redirect_to :back, :alert => "Game not found"
+		end
+
 		@expansion = @edition.expansions.build
+		rescue ActionController::RedirectBackError
+			redirect_to '/', :alert => "Game not found"
+	end
+
+	private
+
+	def expansion_params
+		params.require(:expansion).permit(:title,:description,:release_date, :coverart, :delete_coverart)
 	end
 end
--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,3 +1,15 @@
 class Expansion < ActiveRecord::Base
-  belongs_to :edition
+	has_attached_file :coverart, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :coverart, :content_type => /\Aimage\/.*\Z/
+
+	belongs_to :edition
+	before_validation { coverart.clear if @delete_coverart }
+
+	def delete_coverart
+		@delete_coverart ||= false
+	end
+
+	def delete_coverart=(value)
+		@delete_coverart  = !value.to_i.zero?
+	end
 end
