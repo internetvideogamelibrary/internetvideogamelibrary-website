be042cae7c9fc94f2d146c98e346fb2e32d5dd60
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-02-04 23:38:53 -0200

    Adding expansions to index

--- app/chewy/games_index.rb
+++ app/chewy/games_index.rb
@@ -8,6 +8,19 @@ class GamesIndex < Chewy::Index
 		}
 	}
 
+	define_type Expansion.includes(edition: [:genres, :platform, :region, :work]) do
+		field :title, analyzer: 'title'
+		field :release_date, type: 'date'
+		field :original_title, value: ->{ edition.work.original_title }
+		field :original_release_date, value: ->{ edition.work.original_release_date }
+		field :platform, value: ->{ edition.platform.title }
+		field :platform_id, value: -> { edition.platform_id }, type: 'integer'
+		field :region, value: ->{ edition.region.title }
+		field :region_id, value: ->{ edition.region_id }, type: 'integer'
+		field :description
+		field :genres, index: 'not_analyzed', value: ->{ edition.genres.map(&:title) }
+	end
+
 	define_type Edition.includes(:genres, :platform, :region, :work) do
 		field :title, analyzer: 'title'
 		field :release_date, type: 'date'
