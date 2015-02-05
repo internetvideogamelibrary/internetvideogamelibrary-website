d51c412c03c4b48e971513c0687df97446b1f053
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-02-05 20:00:36 -0200

    Treating empty query string.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,21 @@
 class SearchController < ApplicationController
+	before_filter :has_query,
+	:only => [:search]
 	def search
 		@search = GamesSearch.new(query: params[:q])
 		@games = @search.search.only(:id).load(edition: {scope: Edition.includes(:work)})
+	end
+
+	private
+
+	def has_query
+		if params[:q].present?
+			true
+		else
+			redirect_to :back, :alert => "You have to type a query string"
+		end
+
+		rescue ActionController::RedirectBackError
+			redirect_to editions_path, :alert => "You have to type a query string"
 	end
 end
