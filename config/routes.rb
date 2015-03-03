365da4b1944e90e31002d633bbd603fd07f982d8
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-03-03 08:31:43 -0300

    Adding canonical route (to restrict to one domain)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+	if ENV['CANONICAL_HOST']
+		constraints(:host => Regexp.new("^(?!#{Regexp.escape(ENV['CANONICAL_HOST'])})")) do
+			match "/(*path)" => redirect { |params, req| "http://#{ENV['CANONICAL_HOST']}/#{params[:path]}" },  via: [:get, :post]
+		end
+	end
+
 	resources :games do
 		collection do
 			get 'search'
