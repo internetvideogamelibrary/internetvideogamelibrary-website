074781f302fba0fe74450950d5d5c00229324e12
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-03-20 00:59:24 -0300

    Minor adjustments in thegamesdb parser

--- utils/thegamesdb/parser.rb
+++ utils/thegamesdb/parser.rb
@@ -3,10 +3,16 @@
 require 'open-uri'
 require 'nokogiri'
 
-def safe_release_date(release)
+def super_safe_release_date(release)
 	return Time.strptime(release, "%m/%d/%Y").strftime "%Y-%m-%d"
 rescue ArgumentError
 	return Time.strptime(release, "%Y").strftime "%Y-%m-%d"
+end
+
+def safe_release_date(release)
+	return super_safe_release_date(release)
+rescue ArgumentError
+	return Time.strptime(release, "%B %d, %Y").strftime "%Y-%m-%d"
 end
 
 def open_game(game_url)
@@ -32,17 +38,17 @@ def open_game(game_url)
 
 
 	print <<-endofgame
-work = Work.create(:original_title => "#{title}", :original_release_date => '#{release}')
-edition = Edition.create(:media_id => media.id, :region_id => region.id, :platform_id => platform.id, :work_id => work.id, :description => "#{description}", :release_date => "#{release}", :title => "#{title}", :developer => "#{developer}", :publisher => "#{publisher}"#{cover})
+	work = Work.create(:original_title => "#{title}", :original_release_date => '#{release}')
+	edition = Edition.create(:media_id => media.id, :region_id => region.id, :platform_id => platform.id, :work_id => work.id, :description => "#{description}", :release_date => "#{release}", :title => "#{title}", :developer => "#{developer}", :publisher => "#{publisher}"#{cover})
 endofgame
 
 	genres.each do |genre|
 		print <<-endofgame
-genre = Genre.find_by_title("#{genre}")
-if genre.present?
-	edition.genres << genre
-end
-endofgame
+	genre = Genre.find_by_title("#{genre}")
+	if genre.present?
+		edition.genres << genre
+	end
+	endofgame
 end
 
 	print <<-endofgame
@@ -52,13 +58,18 @@ end
 endofgame
 end
 
-doc = Nokogiri::XML open("http://thegamesdb.net/api/GetPlatformGames.php?platform=41")
+doc = Nokogiri::XML open("http://thegamesdb.net/api/GetPlatformGames.php?platform=3")
 print <<-endofgame
-media = Media.find_by_title("Cartridge")
-region = Region.find_by_title("North America")
-platform = Platform.find_by_title("Nintendo Game Boy Color")
+Chewy.strategy(:atomic) do
+	media = Media.find_by_title("Cartridge")
+	region = Region.find_by_title("North America")
+	platform = Platform.find_by_title("Nintendo 64")
 endofgame
 
 doc.xpath("//Data/Game/id").each do |id|
 	open_game "http://thegamesdb.net/api/GetGame.php?id=#{id.text}"
 end
+
+print <<-endofgame
+end
+endofgame
