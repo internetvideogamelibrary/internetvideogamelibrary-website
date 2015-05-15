81724d6a5e37f4e590571826b4becca4be9794d3
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-05-15 17:46:13 -0300

    Adding about and faq to footer

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,10 @@ class IndexController < ApplicationController
 		results = @search.all.order(created_at: :desc).limit(6).only(:id)
 		@games = results.load
 	end
+
+	def faq
+	end
+
+	def about
+	end
 end
