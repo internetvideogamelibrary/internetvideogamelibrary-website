efe15329708ae890cf5f8960cccf73770ae0a1bf
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-07-05 17:25:21 -0300

    Trying to fix referral spam problems.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
+require 'uri'
+
 class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
-	before_filter :fill_platforms
+	before_filter :fill_platforms, :ignore_referral_spam
+
+	def ignore_referral_spam
+		if(request.referrer)
+			@uri = URI(request.referrer)
+			if(REFERRAL_SPAMMERS.include?(@uri.host))
+				render :nothing => true, :status => 400
+			end
+		end
+	end
 
 	def after_sign_in_path_for(resource)
 		user_game_shelves_path(resource)
