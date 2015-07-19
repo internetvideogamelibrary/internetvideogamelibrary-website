e1c1149f06205fba260daf76229ac68371c9a691
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-07-19 12:04:08 -0300

    Inverting the filter order on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
-	before_filter :fill_platforms, :ignore_referral_spam
+	before_filter :ignore_referral_spam, :fill_platforms
 
 	def ignore_referral_spam
 		if(request.referrer)
