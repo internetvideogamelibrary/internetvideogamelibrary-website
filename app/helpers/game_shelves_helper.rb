99a5191db0fdf22f8562e168fc5f48def615682d
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-07-28 23:47:50 -0300

    refactoring dropdown menu of shelves

--- app/helpers/game_shelves_helper.rb
+++ app/helpers/game_shelves_helper.rb
@@ -32,4 +32,43 @@ module GameShelvesHelper
 		{:shelf => shelf, :shelf_item => shelf_item, :game => game, :checked => (shelf_item.present?),
 			:html_class => html_class, :html_role => html_role, :html_tabindex => html_tabindex, :title => title}
 	end
+
+	def add_shelf_dropdown(user, game)
+		shelves = GameShelf.where(:user_id => user.id).order(:shelf_type).includes(:user)
+		shelf_item = ShelfItem.joins(:game_shelf).where("shelf_type <> ? and user_id = ? and item_type = ? and item_id = ?", GameShelf.shelf_types[:custom], user.id, game.class.name, game.id).first
+
+		render :partial => "shared/add_shelf_dropdown", :locals => {:shelves => shelves, :shelf_item => shelf_item, :game => game}
+	end
+
+	def href_user_game_shelf_path(user, shelf, game, is_item_on_shelf)
+		if is_item_on_shelf == 0
+			add_user_game_shelf_path(user, shelf, game)
+		else
+			remove_item_user_game_shelves_path(shelf.user, :item_id => game.id)
+		end
+	end
+
+	def toggle_href_user_game_shelf_path(user, shelf, game, is_item_on_shelf)
+		if is_item_on_shelf == 1
+			add_user_game_shelf_path(user, shelf, game)
+		else
+			remove_item_user_game_shelves_path(user)
+		end
+	end
+
+	def add_user_game_shelf_path(user, shelf, game)
+		if game.class.name == Edition.name
+			add_edition_user_game_shelf_path(user, shelf, :edition_id => game.id)
+		elsif game.class.name == Expansion.name
+			add_expansion_user_game_shelf_path(user, shelf, :expansion_id => game.id)
+		end
+	end
+
+	def is_item_on_shelf?(shelf_item, game_shelf)
+		if(shelf_item && shelf_item.game_shelf == game_shelf)
+			1
+		else
+			0
+		end
+	end
 end
