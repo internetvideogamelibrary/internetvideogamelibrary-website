b9619db3c8e5e216542a150cd10f15f865ae887e
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-09-04 16:16:03 -0300

    Extracting index default_settings to common class

--- app/chewy/works_index.rb
+++ app/chewy/works_index.rb
@@ -1,46 +1,5 @@
-class WorksIndex < Chewy::Index
-	settings analysis: {
-		filter: {
-			my_synonym_filter: {
-				type: "synonym",
-				synonyms: [
-					"littlebigplanet,little big planet",
-					"megaman,mega man",
-					"i,1",
-					"ii,2",
-					"iii,3",
-					"iv,4",
-					"v,5",
-					"vi,6",
-					"vii,7",
-					"viii,8",
-					"ix,9",
-					"x,10",
-					"xi,11",
-					"xii,12",
-					"xiii,13",
-					"xiv,14",
-					"xv,15",
-					"xvi,16",
-					"xvii,17",
-					"xviii,18",
-					"xix,19",
-					"xx,20",
-					"farcry,far cry",
-					"bioshock,bio shock",
-					"olliolli,olli olli",
-					"actraiser,act raiser",
-					"gta,grand theft auto"
-				]
-			}
-		},
-		analyzer: {
-			title: {
-				tokenizer: 'standard',
-				filter: ['lowercase', 'asciifolding', "my_synonym_filter"]
-			}
-		}
-	}
+class WorksIndex < DefaultIndex
+	settings self.default_settings
 
 	define_type Work do
 		field :original_title, analyzer: 'title'
