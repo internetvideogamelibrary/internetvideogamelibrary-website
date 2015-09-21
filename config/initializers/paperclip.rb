327e433c462bc1459da58e88602074a20b789a06
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-09-20 22:18:14 -0300

    Fix paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,11 +1,9 @@
 if Rails.env.production?
-	Rails.application.config.paperclip_defaults = {
-		:storage => :s3,
-		:s3_credentials => {
-			:bucket => ENV['S3_BUCKET_NAME'],
-			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-			:s3_region => ENV['S3_REGION']
-		}
+	Paperclip::Attachment.default_options[:storage] = :s3
+	Paperclip::Attachment.default_options[:s3_credentials] = {
+		:bucket => ENV['S3_BUCKET_NAME'],
+		:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+		:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+		:s3_region => ENV['S3_REGION']
 	}
 end
