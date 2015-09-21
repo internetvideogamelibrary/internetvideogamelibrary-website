926cc4d8f5a20b01d97a9e4532fe7a6c5cd768b3
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-09-20 21:59:10 -0300

    Add S3 region to paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
 			:bucket => ENV['S3_BUCKET_NAME'],
 			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+			:s3_region => ENV['S3_REGION']
 		}
 	}
 end
