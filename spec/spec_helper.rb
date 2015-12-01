b42506dbde51b241f448739d1d9a8c9ab1eff2b9
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-12-01 11:31:04 -0200

    Update chewy gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require "codeclimate-test-reporter"
+require 'chewy/rspec'
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
