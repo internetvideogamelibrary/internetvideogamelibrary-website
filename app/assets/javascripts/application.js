433cb33583437750f8b12b16f824eadf7a25fcdf
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-12-07 15:27:26 -0200

    Add bower to project and remove gems (WIP) This commit adds a bower.json and start the modifications needed for using bower for controlling frontend dependencies. More testing needed to make sure everything is working as it should.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require bootstrap-sprockets
+//= require jquery.turbolinks/vendor/assets/javascripts/jquery.turbolinks
+//= require jquery-ujs
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
 //= require jquery-readyselector
 //= require_tree .
 //= require turbolinks
