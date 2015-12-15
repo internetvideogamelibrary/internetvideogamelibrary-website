44e6830a647516960ca152e2af85b5f66b4ab52f
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2015-12-15 17:19:11 -0200

    Change home to also use only index to show games.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,10 @@
+require 'games_index_view_object'
+
 class IndexController < ApplicationController
   def show
     @search = GamesSearch.new
-    results = @search.all.order(created_at: :desc).limit(6).only(:id)
-    @games = results.load
+    results = @search.all.order(created_at: :desc).limit(6)
+    @games = GamesIndexViewObject.construct_array_from_chewy_map(results.map)
     @editions_count = @search.editions_count.total
     @expansions_count = @search.expansions_count.total
     @work_count = WorksSearch.new.works_count.total
