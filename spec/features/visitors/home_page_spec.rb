063f95dffdbc31db72a6e97b7175df4c51bbf2ba
Renato "Lond" Cerqueira <renato@lond.com.br>
2016-06-07 22:46:19 +0200

    Add tests for homepage game counts and recently added games.

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -7,8 +7,46 @@ feature 'Home page' do
   #   Given I am a visitor
   #   When I visit the home page
   #   Then I see "Welcome"
+  before(:all) do
+    Timecop.scale(60)
+    WorksIndex.purge!
+    GamesIndex.purge!
+    games = []
+    games << @game1 = FactoryGirl.create(:edition, release_date: Date.new(2016, 5, 12))
+    games << @game2 = FactoryGirl.create(:edition, release_date: Date.new(2015, 5, 12))
+    games << @game3 = FactoryGirl.create(:edition, release_date: Date.new(2014, 5, 12))
+    games << @game4 = FactoryGirl.create(:edition, release_date: Date.new(2013, 5, 12))
+    games << @game5 = FactoryGirl.create(:edition, release_date: Date.new(2012, 5, 12))
+    games << @game6 = FactoryGirl.create(:edition, release_date: Date.new(2011, 5, 12))
+    games << @game7 = FactoryGirl.create(:edition, release_date: Date.new(2010, 5, 12))
+    games << @game8 = FactoryGirl.create(:edition, release_date: Date.new(2009, 5, 12))
+    games << @game9 = FactoryGirl.create(:edition, work: @game8.work, release_date: Date.new(2008, 5, 12))
+    @expansion1 = FactoryGirl.create(:expansion, edition: @game9)
+    work_ids = games.map &->(game) {game.work.id}
+    game_ids = games.map &->(game) {game.id}
+    WorksIndex::Work.import work_ids
+    GamesIndex::Edition.import game_ids
+    GamesIndex::Expansion.import [@expansion1.id]
+  end
+
   scenario 'visit the home page' do
     visit root_path
     expect(page).to have_content 'Welcome'
+  end
+
+  scenario 'check games counter' do
+    visit root_path
+    expect(page).to have_content 'We currently have 8 unique games, 9 editions and 1 Expansions/DLC in our database!'
+  end
+
+  scenario 'check recently added games' do
+    visit root_path
+    games = page.find('#recently_added_games')
+    expect(games).to have_css 'div.col-md-2:nth-of-type(1) div/a', text: @expansion1.title
+    expect(games).to have_css 'div.col-md-2:nth-of-type(2) div/a', text: @game9.title
+    expect(games).to have_css 'div.col-md-2:nth-of-type(3) div/a', text: @game8.title
+    expect(games).to have_css 'div.col-md-2:nth-of-type(4) div/a', text: @game7.title
+    expect(games).to have_css 'div.col-md-2:nth-of-type(5) div/a', text: @game6.title
+    expect(games).to have_css 'div.col-md-2:nth-of-type(6) div/a', text: @game5.title
   end
 end
