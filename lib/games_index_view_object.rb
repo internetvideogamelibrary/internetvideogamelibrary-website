32d6ae7fd8640a2f99c4953ca3bfe819e641bdfe
Renato "Lond" Cerqueira <renato@lond.com.br>
2016-06-07 23:18:49 +0200

    Remove debug print.

--- lib/games_index_view_object.rb
+++ lib/games_index_view_object.rb
@@ -3,7 +3,6 @@ class GamesIndexViewObject
     constructed_array = []
 
     map.each do |item|
-      p item
       constructed_array << GamesIndexViewObject.new(item.title, item.release_date, item.original_title, item.original_release_date, item.work_id, item.platform, item.platform_id, item.region, item.region_id, item.description, item.genres, item.created_at, item.coverart_url, item.edition_id, item.expansion_id, item.edition_slug, item.expansion_slug)
     end
 
