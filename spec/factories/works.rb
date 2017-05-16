33a9e43cafade13e5a0f4caa85ba6d32370d7adc
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-05-16 19:29:32 +0200

    Add work factory with expansions

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -24,5 +24,20 @@ FactoryGirl.define do
         work.reload
       end
     end
+
+    factory :work_with_editions_and_expansions do
+      transient do
+        editions_count 1
+        expansions_count 1
+      end
+
+      after(:create) do |work, evaluator|
+        create_list(:edition, evaluator.editions_count, work: work)
+        work.reload
+        work.editions.each do |e|
+          create_list(:expansion, evaluator.expansions_count, edition: e)
+        end
+      end
+    end
   end
 end
