42f4262388ef6b535d243bd174aec487740c55f0
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-05-16 19:29:59 +0200

    Add simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start
 require 'codeclimate-test-reporter'
 require 'chewy/rspec'
 require 'capybara/rspec'
